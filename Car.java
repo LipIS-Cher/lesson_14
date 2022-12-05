196b653fa5a436a312d071281e35ca1af6d91a43
Ivan <lipatow.ivan2016@yandex.ru>
2022-12-06 00:58:09 +0300

    Third homework.

--- Car.java
+++ Car.java
@@ -12,12 +12,36 @@ public class Car {
     String country;
 
     Car(String brand, String model, float engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        if (brand == null){
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model == null){
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (engineVolume <= 0){
+            this.engineVolume = 1.5f;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null){
+            this.color = "белый";
+        } else {
+            this.color = color;
+        }
+        if (year <= 0){
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
+        if (country == null){
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
     }
 
     @Override
